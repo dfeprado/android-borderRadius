44506d864b80a0e8ea2d6576ddc4367488704db0
Daniel Prado <dfe.prado@gmail.com>
2021-06-10 21:10:08 -0300

    Corrige o bug das bordas e adiciona o README.md; altera o ícone da aplicação

---
+++ README.md
@@ -0,0 +1,6 @@
+# BorderRadius
+A simple CSS border-radius property visualizer, for Android
+
+<div style="text-align: center">
+    <img src="./screenshot.png" style="width: 350px;"/>
+</div>
--- app/src/main/java/dev/danielprado/borderradius/BoxBorderRadius.kt
+++ app/src/main/java/dev/danielprado/borderradius/BoxBorderRadius.kt
@@ -10,11 +10,12 @@ import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
 
+private data class RadiusSize(val horizontal: Float, val vertical: Float)
+
 class BoxBorderRadius(context: Context, attrs: AttributeSet?): View(context, attrs) {
-    private var topLeftRadius = 0f
-    private var topRightRadius = 0f
-    private var bottomRightRadius = 0f
-    private var bottomLeftRadius = 0f
+    private var horizontalCenter = 0f
+    private var verticalCenter = 0f
+    private var minCenter = 0f
     private var path: Path? = null
     private var borderRadiusString = ""
     private var borderRadiusRegex = Regex("(?:(\\d+)(px|em|%)){1,4}", RegexOption.IGNORE_CASE)
@@ -28,7 +29,8 @@ class BoxBorderRadius(context: Context, attrs: AttributeSet?): View(context, att
         context.theme.obtainStyledAttributes(attrs, R.styleable.BoxBorderRadius, 0, 0).apply {
             try {
                 paint.color = getColor(R.styleable.BoxBorderRadius_bkgColor, Color.BLACK)
-                parseBorderRadius(getString(R.styleable.BoxBorderRadius_borderRadius) ?: "")
+                borderRadiusString = getString(R.styleable.BoxBorderRadius_borderRadius) ?: ""
+                buildPath(parseBorderRadius())
             }
             finally {
                 recycle()
@@ -47,109 +49,116 @@ class BoxBorderRadius(context: Context, attrs: AttributeSet?): View(context, att
     var borderRadius: String
         get() = this.borderRadiusString
         set(value) {
-            parseBorderRadius(value)
             borderRadiusString = value
-            buildPath()
+            buildPath(parseBorderRadius())
             invalidate()
         }
 
-    private fun parseBorderRadius(value: String) {
-        if (value.isNotEmpty()) {
-            val matches = borderRadiusRegex.findAll(value)
-            when(matches.count()) {
+    private fun parseBorderRadius(): Array<RadiusSize> {
+        if (borderRadiusString.isNotEmpty()) {
+            val matches = borderRadiusRegex.findAll(borderRadiusString)
+            return when (matches.count()) {
                 4 -> setRadiusFrom4Values(matches)
                 3 -> setRadiusFrom3Values(matches)
                 2 -> setRadiusFrom2Values(matches)
-                1 -> setRadiusFrom1Value(matches)
+                else -> setRadiusFrom1Value(matches)
             }
 
         } else {
-            topLeftRadius = 0f
-            topRightRadius = 0f
-            bottomLeftRadius = 0f
-            bottomRightRadius = 0f
+            return arrayOf(
+                    RadiusSize(0f, 0f),
+                    RadiusSize(0f, 0f),
+                    RadiusSize(0f, 0f),
+                    RadiusSize(0f, 0f)
+            )
         }
     }
 
-    private fun getRadiusValue(match: MatchResult): Float {
+    private fun newRadiusSizeFromPercentage(horizontal: Float, vertical: Float): RadiusSize {
+        val hSize = if (horizontal <= horizontalCenter) horizontal else horizontalCenter
+        val vSize = if (vertical <= verticalCenter) vertical else verticalCenter
+        return RadiusSize(hSize, vSize)
+    }
+
+    private fun newRadiusSizeFromValue(value: Float): RadiusSize {
+        val size = minOf(value, minCenter)
+        return RadiusSize(size, size)
+    }
+
+    private fun getRadiusValue(match: MatchResult): RadiusSize {
         val unit = match.groupValues[2]
         val value = match.groupValues[1].toFloat()
         return when(unit) {
-            "em" -> TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, displayMetrics)
-            "%" -> width * value / 100 // %
-            else -> value // px
+            "em" -> newRadiusSizeFromValue(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, displayMetrics))
+            "%" -> newRadiusSizeFromPercentage(width/2 * value/100, height/2 * value/100) // %
+            else -> newRadiusSizeFromValue(value) // px
         }
     }
 
-    private fun setRadiusFrom4Values(matches: Sequence<MatchResult>) {
-        topLeftRadius = getRadiusValue(matches.elementAt(0))
-        topRightRadius = getRadiusValue(matches.elementAt(1))
-        bottomRightRadius = getRadiusValue(matches.elementAt(2))
-        bottomLeftRadius = getRadiusValue(matches.elementAt(3))
+    private fun setRadiusFrom4Values(matches: Sequence<MatchResult>): Array<RadiusSize> {
+        return arrayOf(
+                getRadiusValue(matches.elementAt(0)),
+                getRadiusValue(matches.elementAt(1)),
+                getRadiusValue(matches.elementAt(2)),
+                getRadiusValue(matches.elementAt(3))
+        )
     }
 
-    private fun setRadiusFrom3Values(matches: Sequence<MatchResult>) {
-        topLeftRadius = getRadiusValue(matches.elementAt(0))
-        topRightRadius = getRadiusValue(matches.elementAt(1))
-        bottomLeftRadius = topRightRadius
-        bottomRightRadius = getRadiusValue(matches.elementAt(2))
+    private fun setRadiusFrom3Values(matches: Sequence<MatchResult>): Array<RadiusSize> {
+        val topRightBottomLeft = getRadiusValue(matches.elementAt(1))
+        return arrayOf(
+                getRadiusValue(matches.elementAt(0)),
+                topRightBottomLeft,
+                getRadiusValue(matches.elementAt(2)),
+                topRightBottomLeft
+        )
     }
 
-    private fun setRadiusFrom2Values(matches: Sequence<MatchResult>) {
-        topLeftRadius = getRadiusValue(matches.elementAt(0))
-        bottomRightRadius = topLeftRadius
-        topRightRadius = getRadiusValue(matches.elementAt(1))
-        bottomLeftRadius = topRightRadius
+    private fun setRadiusFrom2Values(matches: Sequence<MatchResult>): Array<RadiusSize> {
+        val topLeftBottomRight = getRadiusValue(matches.elementAt(0))
+        val topRightBottomLeft = getRadiusValue(matches.elementAt(1))
+        return arrayOf(
+                topLeftBottomRight,
+                topRightBottomLeft,
+                topLeftBottomRight,
+                topRightBottomLeft
+        )
     }
 
-    private fun setRadiusFrom1Value(matches: Sequence<MatchResult>) {
-        topLeftRadius = getRadiusValue(matches.elementAt(0))
-        topRightRadius = topLeftRadius
-        bottomRightRadius = topLeftRadius
-        bottomLeftRadius = topLeftRadius
+    private fun setRadiusFrom1Value(matches: Sequence<MatchResult>): Array<RadiusSize> {
+        val allRadius = getRadiusValue(matches.elementAt(0));
+        return arrayOf(
+                allRadius,
+                allRadius,
+                allRadius,
+                allRadius
+        )
     }
 
-    private fun limitBorderRadius() {
-        val controlValue = if (width > height) height.toFloat()/2 else width.toFloat()/2
-
-        if (topRightRadius > controlValue)
-            topRightRadius = controlValue - 1
-
-        if (topLeftRadius > controlValue)
-            topLeftRadius = controlValue - 1
-
-        if (bottomRightRadius > controlValue)
-            bottomRightRadius = controlValue - 1
-
-        if (bottomLeftRadius > controlValue)
-            bottomRightRadius = controlValue - 1
-    }
-
-    private fun buildPath() {
-        limitBorderRadius()
+    private fun buildPath(radius: Array<RadiusSize>) {
         val width = this.width.toFloat() - 1
         val height = this.height.toFloat() - 1
         path = Path().apply {
-            moveTo(0 + topLeftRadius, 0f)
+            moveTo(0 + radius[0].horizontal, 0f)
 
             // top line
-            lineTo(width - topRightRadius, 0f)
+            lineTo(width - radius[1].horizontal, 0f)
             // top right radius
-            cubicTo(width - topRightRadius, 0f, width, 0f, width, 0 + topRightRadius)
+            cubicTo(width - radius[1].horizontal, 0f, width, 0f, width, 0 + radius[1].vertical)
 
             // right line
-            lineTo(width, height - bottomRightRadius);
+            lineTo(width, height - radius[2].vertical);
             // bottom right radius
-            cubicTo(width, height - bottomRightRadius, width, height, width - bottomRightRadius, height)
+            cubicTo(width, height - radius[2].vertical, width, height, width - radius[2].horizontal, height)
 
             // bottom line
-            lineTo(0 + bottomLeftRadius, height);
+            lineTo(0 + radius[3].horizontal, height);
             // bottom left radius
-            cubicTo(0f + bottomLeftRadius, height, 0f, height, 0f, height - bottomLeftRadius)
+            cubicTo(0f + radius[3].horizontal, height, 0f, height, 0f, height - radius[3].vertical)
 
             // left line
-            lineTo(0f, 0f + topLeftRadius)
-            cubicTo(0f, 0f + topLeftRadius, 0f, 0f, 0f + topLeftRadius, 0f)
+            lineTo(0f, 0f + radius[0].vertical)
+            cubicTo(0f, 0f + radius[0].vertical, 0f, 0f, 0f + radius[0].horizontal, 0f)
         }
     }
 
@@ -163,6 +172,9 @@ class BoxBorderRadius(context: Context, attrs: AttributeSet?): View(context, att
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        buildPath()
+        horizontalCenter = w.toFloat()/2
+        verticalCenter = h.toFloat()/2
+        minCenter = minOf(horizontalCenter, verticalCenter)
+        buildPath(parseBorderRadius())
     }
 }
